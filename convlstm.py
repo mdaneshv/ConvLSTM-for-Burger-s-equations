0918a1c41ff034c24dce9d59a132ceb91124d9bb
mdaneshv <58681626+mdaneshv@users.noreply.github.com>
2020-04-30 03:00:32 -0500

    Update convlstm.py

--- convlstm.py
+++ convlstm.py
@@ -10,13 +10,11 @@ from keras import optimizers
 from keras import backend as K
 import seaborn as sns
 import pandas.util.testing as tm
+from urllib.request import urlretrieve
 
 
-# Original data
-Origin_data = np.genfromtxt('U16.dat', delimiter=' ')
-
-# Define encoder-decoder
 '''
+# Define encoder-decoder
 def encoder_decoder(data, code_size):
   features = data.shape[1]
   m = data.shape[0]
@@ -39,15 +37,23 @@ def encoder_decoder(data, code_size):
 
   dataset = autoencoder.predict(data)  # denoise data
 
-  return dataset
+  return dataset  
+
 '''
+
+Origin_data = np.genfromtxt('U16.dat', delimiter=' ')
+
+for i in range(16):
+        Origin_data[:, i] = preprocessing.scale(Origin_data[:, i])
+
+
 # Create datasets for ConvLSTM2d
 def ConvLSTM_dataset(dataset, time_window, rows, columns):
     features = dataset.shape[0] 
     m = dataset.shape[1] 
-    samples = int(m / columns) - 1 # number of images 
-    test_size = 5                  # the true test size for images will become: test_size - time_window
-    train_size = samples - test_size
+    samples = int(m / columns)   # number of images 
+    test_size = 10
+    train_size = samples - test_size - time_window
     
 
     # start converting dataset into 2D-images
@@ -70,10 +76,10 @@ def ConvLSTM_dataset(dataset, time_window, rows, columns):
     Y = Z[time_window:, :, :]  # target values for X
 
     # creating train and test sets and corresponding target values from X and Y
+              
     X_train = X[:train_size, :]
     Y_train = Y[:train_size, :]
     X_test = X[train_size:train_size + test_size, :]
-    X_test = np.delete(X_test, (test_size - time_window), axis=0)
     Y_test = Y[train_size:train_size + test_size, :]
 
     # reshape input and output to be fed into ConvLastm2D layers: input must be 5 dimensional 
@@ -81,7 +87,7 @@ def ConvLSTM_dataset(dataset, time_window, rows, columns):
     Xtest_set = X_test.reshape((X_test.shape[0], time_window, rows, columns, 1))
     Ytrain_set = Y_train.reshape((Y_train.shape[0], rows, columns, 1))
     Ytest_set = Y_test.reshape((Y_test.shape[0], rows, columns, 1))
-
+  
     return Xtrain_set, Ytrain_set, Xtest_set, Ytest_set
 
 # Define ConvLSTM2D model
@@ -97,7 +103,12 @@ def create_ConvLSTM_layers(X, Y, filters, kernel_size, batch_size, epochs, learn
     model.add(ConvLSTM2D(filters=filters, kernel_size=kernel_size,
                          input_shape=(time_window, rows, columns, 1),
                          padding='same', return_sequences=True))
-  
+    
+    model.add(ConvLSTM2D(filters=filters, kernel_size=kernel_size,
+                         input_shape=(time_window, rows, columns, 1),
+                         padding='same', return_sequences=True))
+    
+    
     # last layer has 1 filter
     model.add(ConvLSTM2D(filters=1, kernel_size=kernel_size,    
                          input_shape=(time_window, rows, columns, 1),
@@ -106,7 +117,7 @@ def create_ConvLSTM_layers(X, Y, filters, kernel_size, batch_size, epochs, learn
 
     adam = optimizers.Adam(lr=learning_rate, beta_1=0.99, beta_2=0.999)
 
-    model.compile(loss='mse', optimizer=adam #metrics=['accuracy'])
+    model.compile(loss='mse', optimizer=adam, metrics=['accuracy'])
 
     # fit model to the data
     history = model.fit(X, Y, epochs=epochs, batch_size=batch_size, verbose=2, shuffle=True)
@@ -136,28 +147,26 @@ def prediction(model, X, time_window, rows, columns):
     return Predictions
 
 # Paste columns of Predictions and true values to create 2 vectors for plotting
-def make_plots(Predictions, Ytest_set, pred_steps):
-    Ypred = {}
-    Ytest = {}
+def make_plots(Predictions,Ytest_set, pred_steps):
+    Ypred = np.zeros((rows,pred_steps*columns))
+    Ytest = np.zeros((rows,pred_steps*columns))
 
     for i in range(rows):
-        Ypred[i] = Predictions[0, i, :, 0]
-        Ytest[i] = Ytest_set[0, i, :, 0]
-        for j in range(0, pred_steps):
-            Ypred[i] = np.append(Ypred[i], Predictions[j, i, :, 0])
-            Ytest[i] = np.append(Ytest[i], Ytest_set[j, i, :, 0])
+        for j in range(pred_steps):
+            Ypred[i,j*columns:j*columns+columns] =  Predictions[j, i, :, 0]
+            Ytest[i,j*columns:j*columns+columns] =  Ytest_set[j, i, :, 0]
 
     # plots  
     for i in range(rows):
-        plot1, = plt.plot(Ytest[i])
+        plot1, = plt.plot(Ytest[i,:])
 
-        plot2, = plt.plot(Ypred[i])
+        plot2, = plt.plot(Ypred[i,:])
 
-        plt.xlabel('steps')
+        plt.xlabel('prediction horizon')
 
-        plt.ylabel('$X[%i]$' % i)
+        plt.ylabel('$x[%i]$' % i)
 
-        plt.title('Prediction for feature $%i$' % i, fontsize=10)
+        plt.title('Prediction for feature $x[%i]$ using ConvLSTM' % i, fontsize=10)
 
         plt.legend([plot1, plot2], ["true_values", "prediction"])
 
@@ -168,17 +177,15 @@ def make_plots(Predictions, Ytest_set, pred_steps):
     return Ypred, Ytest, plot1, plot2
 
 
-time_window = 2  # recurrent steps or tiem-steps
-rows = 16        # I chose it to be the same as number of features
-columns = 3      # columns of images 
-pred_steps = 3   # prediction horizon shown on x_axis of plots which is : pred_steps * columns 
-# pred_steps < test_seize - time_window
-filters = 30     # number of filters in convolutional layres
+time_window = 3    # recurrent steps or tiem-steps
+rows = 16          # I chose it to be the same as number of features
+columns = 50       # columns of images 
+pred_steps = 10    # prediction horizon shown on x_axis of plots which is : pred_steps * columns 
+filters = 30       # number of filters in convolutional layres
 kernel_size = (10,3)
-batch_size = 128
-epochs = 7
+batch_size = 512
+epochs = 20
 learning_rate = 0.003
-code_size = 10   # number of hidden units in encoder-decoder
 
 #dataset = encoder_decoder(np.transpose(Origin_data), code_size)
 Xtrain_set, Ytrain_set, Xtest_set, Ytest_set = ConvLSTM_dataset(Origin_data, time_window, rows, columns)
@@ -186,3 +193,4 @@ model, history = create_ConvLSTM_layers(Xtrain_set, Ytrain_set, filters, kernel_
 Predictions = prediction(model, Xtest_set, time_window, rows, columns)
 Ypred, Ytest, plot1, plot2 = make_plots(Predictions, Ytest_set, pred_steps)
 
+### prediction starts from element (samples-test_size)*columns from Origin_data
