187f883c07eb2b5f1a9027cfa1d2d3dc810aa63c
Mohammad Daneshvar <58681626+mdaneshv@users.noreply.github.com>
2020-11-28 04:02:50 -0600

    Update Neural Network.py

--- Machine Learning /Neural Network.py
+++ Machine Learning /Neural Network.py
@@ -5,28 +5,27 @@ import random
 
 def initial(X, Y, n_h):
     
-    n = np.shape(X)[0]
-    m = np.shape(X)[1]
+    (n,m) = np.shape(X)
     n_y = np.shape(Y)[0]
     w1 = np.zeros((n_h, n))
     w2 = np.zeros((n_y, n_h))
     b1 = np.zeros((n_h, m))
     b2 = np.zeros((n_y, m))
-    return w1, w2, b1, b2
+    return [w1, w2, b1, b2]
 
 
-def FP(X, w1, w2, b1, b2):
+def forward_propagation(X, w1, w2, b1, b2):
     
-    Z1 = np.dot(w1,X) + b1   # (n_h,m)
+    Z1 = np.dot(w1, X) + b1   # (n_h,m)
     A1 = np.tanh(Z1)   #(n_h,m)
-    Z2 = np.dot(w2,A1) + b2   #(n_y,m)
+    Z2 = np.dot(w2, A1) + b2   #(n_y,m)
     A2 = 1/(1 + np.exp(-Z2))   #(n_y,m)
     parameters = {"Z1":Z1, "Z2":Z2, "A2":A2, "A1":A1}
     return parameters
 
 
-def cost(X, Y, A, w1,
-         w2, b1, b2):
+def loss_function(X, Y, A, w1,
+                  w2, b1, b2):
     
     m = int(np.shape(X)[0])
     log1 = np.multiply(Y, np.log(A))
@@ -35,7 +34,7 @@ def cost(X, Y, A, w1,
     return loss
 
     
-def BP(X, Y, w1, w2, parameters):
+def back_propagation(X, Y, w1, w2, parameters):
      
     m = np.shape(X)[0]
     A1 = parameters["A1"]
@@ -47,7 +46,7 @@ def BP(X, Y, w1, w2, parameters):
     dZ_1 = np.multiply(np.dot(w2.T, dZ_2),(1 - np.power(A1, 2)))   #(n_h,m)
     dW_1 = (1/m)*np.dot(dZ_1, X.T)  #(n_h,n_x)
     db_1 = (1/m)*np.sum(dZ_1, axis=1, keepdims=True)
-    return dW_1, dW_2, db_1, db_2
+    return [dW_1, dW_2, db_1, db_2]
 
     
 lr = 20
@@ -60,38 +59,38 @@ X = np.array([[1, 2, -1, -0.1],
               [-1, 0.5, 0, -2]])
 Y = np.array([[1, 1, 0, 0]])
 m = np.shape(X)[1]
-w1, w2, b1, b2 = initial(X, Y, n_h) 
+[w1, w2, b1, b2] = initial(X, Y, n_h) 
 l = [] 
 
 
+# Train the network: apply gradient descent method
 for i in range(0,300):
-   
-    parameters = FP(X, w1, w2, b1, b2)
+    
+    parameters = forward_propagation(X, w1, w2, b1, b2)
     A2 = parameters["A2"]
-    loss = cost(X, Y, A2, w1,
-                w2, b1, b2)
-    l = np.append(l,loss)
-    dW_1, dW_2, db_1, db_2 = BP(X, Y, w1, w2,
-                                parameters)
+    loss = loss_function(X, Y, A2, w1,
+                         w2, b1, b2)
+    l.append(loss)
+    [dW_1, dW_2, db_1, db_2] = back_propagation(X, Y, w1, w2,
+                                                parameters)
     
     w1 = (1 - reg/m)*w1 - lr*dW_1
     w2 = (1 - reg/m)*w2 - lr*dW_2
     b1 = b1 - lr*db_1
     b2 = b2 - lr*db_2
 
-
-X_test = X = np.array([[0, 1, -1, 0.1],
+    
+ # Predict new samples
+X_test = np.array([[0, 1, -1, 0.1],
               [-2, -1, 0, 0],
               [1, -0.5, -1, 2]])
 
-parameters = FP(X_test, w1, w2, b1, b2) 
-
+parameters = forward_propagation(X_test, w1, w2, b1, b2) 
 A2 = parameters["A2"]
 
 print('predict=', A2)
 
-iteration = np.arange(0,300)
+iteration = np.linspace(0,300,300)
 plt.plot(iteration,l)
 plt.title('loss')
 plt.show()
-
